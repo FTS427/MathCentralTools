c7ae275f603a2ec258a41fada204e3a447c0b732
FTS427 <FTS427@outlook.com>
2023-05-07 21:50:30 +0800

    Update

--- Insider/MCT-Linux-Insider.cpp
+++ Insider/MCT-Linux-Insider.cpp
@@ -306,7 +306,7 @@ void M()
         {
             clear();
             h2();
-            s("MCT Insider v0.0.1.0 Beta03");
+            s("MCT Insider v0.0.1.0 Beta04");
             s("Made by FTS427");
             s("Github : https://github.com/FTS427/MathCentTools");
             h2();
